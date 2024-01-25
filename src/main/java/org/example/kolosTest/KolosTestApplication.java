884b2e790084df823a242cb77e261a4d44442a93
mvr3k <61803073+mvr33k@users.noreply.github.com>
2024-01-25 22:00:27 +0100

    porządek w mainie

--- src/main/java/org/example/kolosTest/KolosTestApplication.java
+++ src/main/java/org/example/kolosTest/KolosTestApplication.java
@@ -27,12 +27,9 @@ public class KolosTestApplication {
 
 	public void execProcess() {
 
-		Ingredient ing1=new Ingredient("salami",1);
-		Ingredient ing2=new Ingredient("woda",1);
 		List<Ingredient> ingredientList = new ArrayList<>();
-		ingredientList.add(ing2);
-		ingredientList.add(ing1);
-
+		ingredientList.add(new Ingredient("salami",1));
+		ingredientList.add(new Ingredient("woda",1));
 		orderService.placeOrder(ingredientList, "Lipinki Łużyckie", 12);
 		System.out.println(orderService.checkOrderStatus(1));
 		System.out.println(orderService.cancelOrder(1));
@@ -40,21 +37,21 @@ public class KolosTestApplication {
 
 		System.out.println("\n\n\n");
 
-		Ingredient ing3=new Ingredient("szklanka",3);
-		Ingredient ing4=new Ingredient("stół",5);
-		Ingredient ing5=new Ingredient("pilot",12);
-		Ingredient ing6=new Ingredient("książka",22);
+
 
 		List<Ingredient> ingredientList2 = new ArrayList<>();
-		ingredientList2.add(ing3);
-		ingredientList2.add(ing4);
-		ingredientList2.add(ing5);
-		ingredientList2.add(ing6);
+		ingredientList2.add(new Ingredient("szklanka",3));
+		ingredientList2.add(new Ingredient("stół",5));
+		ingredientList2.add(new Ingredient("pilot",12));
+		ingredientList2.add(new Ingredient("książka",22));
 
-		orderService.placeOrder(ingredientList2, "Lipinki Łużyckie", 12);
+		orderService.placeOrder(ingredientList2, "Lipinki Łużyckie", 55);
 		System.out.println(orderService.checkOrderStatus(2));
 		System.out.println(orderService.cancelOrder(2));
 		System.out.println(orderService.confirmDelivery(2));
+
+
+
 
 	}
 
